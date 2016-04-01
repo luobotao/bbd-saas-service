d7968e29ea1760ecbb905713ad4ec2db55dee1bb
luobotao <botaoluo@gmail.com>
2016-04-01 10:52:48 +0800

    init

---
+++ bbd-saas-provider/src/test/java/com/bbt/demo/provider/ProviderConfig.java
@@ -0,0 +1,14 @@
+package com.bbt.demo.provider;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ *@author zjb
+ *Mar 14, 201610:19:18 AM
+ */
+@Configuration
+@ImportResource({"classpath:META-INF/spring/bbd-saas-provider.xml"})
+public class ProviderConfig {
+
+}
