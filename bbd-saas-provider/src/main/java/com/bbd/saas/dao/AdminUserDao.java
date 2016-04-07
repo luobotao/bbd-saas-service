0b4caa99e4a279712415a5b50bc1df399d21437c
luobotao <botaoluo@gmail.com>
2016-04-07 16:47:05 +0800

    init

--- bbd-saas-provider/src/main/java/com/bbd/saas/dao/AdminUserDao.java
+++ bbd-saas-provider/src/main/java/com/bbd/saas/dao/AdminUserDao.java
@@ -1,14 +1,21 @@
 package com.bbd.saas.dao;
 
 
-import com.bbd.saas.api.AdminUserService;
 import com.bbd.saas.mongoModels.AdminUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
 
 /**
  * Created by luobotao on 2016/4/1.
  * 管理员接口
  */
+@Repository
 public class AdminUserDao {
+    public static final Logger logger = LoggerFactory.getLogger(AdminUserDao.class);
+
+
     /**
      * 根据用户名查找是否存在此管理员
      * @param userName
@@ -18,6 +25,8 @@ public class AdminUserDao {
         AdminUser adminUser = new AdminUser();
         adminUser.setAppkey("123123");
         adminUser.setPassWord("test");
+
+
         return adminUser;
     }
 }
