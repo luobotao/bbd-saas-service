6de469264b29a4a0d736665bf4a0671d7ac18d7e
liyanei <liyanlei@neolix.cn>
2016-04-14 09:35:18 +0800

    覆盖本地

--- bbd-saas-consumer/src/main/webapp/resources/javascripts/page/pageBar.js
+++ bbd-saas-consumer/src/main/webapp/resources/javascripts/page/pageBar.js
@@ -4,39 +4,34 @@ function paginNav(currPage, totalPage, count){
 	var pagestr = "";
 	pagestr += "<div class='col-xs-6'>"; /* pagecon pagination pagination-centered */
 	pagestr += "<div class='dataTables_info' id='userTable_info'>页码：";
-	pagestr += currPage + "/" + totalPage + " &nbsp;&nbsp; 共计" + count + "条记录";
+	pagestr += (parseInt(currPage) + 1) + "/" + totalPage + " &nbsp;&nbsp; 共计" + count + "条记录";
 	pagestr += "</div></div>";
 	pagestr += "<div>";
 	pagestr += "<div class='dataTables_paginate paging_bootstrap'>";
 	pagestr += "<ul class='pagination'>";
-	
-	if (currPage <= 1){
+
+	if (currPage < 1){
 		pagestr += "<li class='prev disabled'><a href='#'>首页</a></li>";
 		pagestr += "<li class='prev disabled'><a href='#'>上一页</a></li>";
 	}else{
-		pagestr += "<li class='prev'><a href='#' onclick=\"gotoPage('1');\">首页</a></li>";
+		pagestr += "<li class='prev'><a href='#' onclick=\"gotoPage('0');\">首页</a></li>";
 		pagestr += "<li class='prev'><a  href='#' onclick=\"gotoPage('" + (parseInt(currPage) + 1) + "');\" title='上一页'>上一页</a></li>";
 	}
-	if(currPage == 1){
-		pagestr += "<li class='active'><a  href='#'>1</a></li>";
-	}else{
-		pagestr += "<li><a href='#' onclick=\"gotoPage('1');\">1</a></li> ";
-	}
-	
+
 	if (totalPage > 1){
 		if (currPage<=5){
-			pagestr += setPageString(2, currPage, currPage);
+			pagestr += setPageString(1, currPage, currPage);
 		}else{
-			 pagestr += "<li><a>...</a></li>";
-			 pagestr += setPageString((parseInt(currPage) - 3) , currPage, currPage);
+			pagestr += "<li><a>...</a></li>";
+			pagestr += setPageString((parseInt(currPage) - 3) , currPage, currPage);
 		}
-		
+
 		if (currPage>=totalPage-4 || totalPage-4<=0){
 			pagestr += setPageString((parseInt(currPage) + 1), totalPage, currPage);
 		}else{
-			  pagestr += setPageString((parseInt(currPage) + 1), (parseInt(currPage) + 3), currPage);
-			  pagestr += "<li><a>...</a></li>";
-			  pagestr += "<li><a href='#' onclick=\"gotoPage('"+totalPage+"\');\"'>"+totalPage+"</a></li> ";
+			pagestr += setPageString((parseInt(currPage) + 1), (parseInt(currPage) + 3), currPage);
+			pagestr += "<li><a>...</a></li>";
+			pagestr += "<li><a href='#' onclick=\"gotoPage('"+totalPage+"\');\"'>"+totalPage+"</a></li> ";
 		}
 	}
 	if (currPage >= totalPage){
@@ -55,10 +50,10 @@ function paginNav(currPage, totalPage, count){
 function setPageString(start, end, currPage){
 	var numstr = "";
 	for(var i = start; i <= end; i ++){
-		if(currPage == i){
+		if(currPage == (i-1)){
 			numstr += "<li class='active'><a href='#'>" + i + "</a></li>";
 		}else{
-			numstr += "<li><a href='#' onclick=\"gotoPage('" + i + "');\">" + i + "</a></li>";
+			numstr += "<li><a href='#' onclick=\"gotoPage('" + (i-1) + "');\">" + i + "</a></li>";
 		}
 	}
 	return numstr;
