eb85aa3d4c54ff0bc81ada4b4a77190e310e4b76
liyanei <liyanlei@neolix.cn>
2016-04-15 17:39:35 +0800

    列表数据查询

--- bbd-saas-api/src/main/java/com/bbd/saas/vo/OrderQueryVO.java
+++ bbd-saas-api/src/main/java/com/bbd/saas/vo/OrderQueryVO.java
@@ -10,7 +10,13 @@ public class OrderQueryVO implements Serializable{
 
 	public String areaCode;//站点编码
 	public Integer arriveStatus;//-1全部 0未到站 1已到站
-	public String between;
+	public String between;//预计到站时间
 	public String parcelCode;//包裹号
 	public String mailNum;//运单号
+	public String userId;//派件员Id
+	public Integer dispatchStatus;//运单分派状态，-1全部, 1-未分派,2-已分派
+	public String arriveBetween;//到站时间
+	public Integer abnormalStatus;//订单异常状态，-1全部 3-滞留,4-拒收
+	public Integer orderStatus;//订单状态，-1全部, 0-未到站,1-未分派,2-已分派,3-滞留,4-拒收,5-已签收,6-已转其他快递,7-申请退货,8-退货完成;
+
 }
--- bbd-saas-consumer/src/main/java/com/bbd/saas/controllers/PackageDispatchController.java
+++ bbd-saas-consumer/src/main/java/com/bbd/saas/controllers/PackageDispatchController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +23,12 @@ import com.bbd.saas.api.OrderService;
 import com.bbd.saas.api.UserService;
 import com.bbd.saas.constants.UserSession;
 import com.bbd.saas.enums.ExpressStatus;
+import com.bbd.saas.enums.OrderStatus;
 import com.bbd.saas.mongoModels.Order;
 import com.bbd.saas.mongoModels.User;
 import com.bbd.saas.utils.FormatDate;
+import com.bbd.saas.utils.NumberUtil;
 import com.bbd.saas.utils.PageModel;
-import com.bbd.saas.utils.StrTool;
 import com.bbd.saas.vo.OrderQueryVO;
 import com.bbd.saas.vo.UserVO;
 
@@ -48,7 +50,7 @@ public class PackageDispatchController {
 	 * Description: 跳转到包裹分派页面
 	 * @param currPage 当前页
 	 * @param status 状态
-	 * @param between 到站时间
+	 * @param arriveBetween 到站时间
 	 * @param courier 派件员
 	 * @param redirectAttrs
 	 * @param model
@@ -57,48 +59,36 @@ public class PackageDispatchController {
 	 * 2016年4月12日下午3:25:07
 	 */
 	@RequestMapping(value="", method=RequestMethod.GET)
-	public String index(Integer pageIndex, Integer status, String between, String courierId, final HttpServletRequest request, Model model) {
-		User user = adminService.get(UserSession.get(request));//当前登录的用户信息
-		between = StrTool.initStr(between, FormatDate.getBetweenTime(new Date(), -2));
-		PageModel<Order> orderPage = getList(pageIndex, status, between, courierId);
-		logger.info(orderPage+"=========");
+	public String index(Integer pageIndex, Integer status, String arriveBetween, String courierId, final HttpServletRequest request, Model model) {
+		//设置默认查询条件
+		status = NumberUtil.defaultIfNull(status, OrderStatus.NOTDISPATCH.getStatus());//未分派
+		//到站时间前天、昨天和今天
+		arriveBetween = StringUtils.defaultIfBlank(arriveBetween, FormatDate.getBetweenTime(new Date(), -2));
+		//查询数据
+		PageModel<Order> orderPage = getList(pageIndex, status, arriveBetween, courierId, request);
+		logger.info("=====运单分派====" + orderPage);
 		model.addAttribute("orderPage", orderPage);
-		model.addAttribute("between", between);
-		
+		model.addAttribute("arriveBetween", arriveBetween);
 		return "page/packageDispatch";
 	}
 	
 	//分页Ajax更新
 	@ResponseBody
 	@RequestMapping(value="/getList", method=RequestMethod.GET)
-	public PageModel<Order> getList(Integer pageIndex, Integer status, String between, String courierId) {
-		if(pageIndex == null){
-			pageIndex = 0;
-		}
-		if(status == null){
-			status = -1;
-		}
-		//功能做完需要删除between
-		if(between != null){
-			between = null;
-		}
-		
-		PageModel<Order> pageModel = new PageModel<>();
-		pageModel.setPageSize(10);
-		pageModel.setPageNo(pageIndex);
-		
+	public PageModel<Order> getList(Integer pageIndex, Integer status, String arriveBetween, String courierId, final HttpServletRequest request) {
+		//参数为空时，默认值设置
+		pageIndex = NumberUtil.defaultIfNull(pageIndex, 0);
+		status = NumberUtil.defaultIfNull(status, -1);
+		//当前登录的用户信息
+		User user = adminService.get(UserSession.get(request));
+		//设置查询条件
 		OrderQueryVO orderQueryVO = new OrderQueryVO();
-		orderQueryVO.arriveStatus = status;
-		orderQueryVO.between = between;
-		
-		PageModel<Order> orderPage = orderService.findOrders(pageModel, orderQueryVO);
-		List<Order> datas = orderPage.getDatas();
-		User user = new User();
-		user.setRealName("张XX");
-		user.setPhone("13256478978");
-		for(Order order : datas){
-			order.setUser(user);
-		}
+		orderQueryVO.dispatchStatus = status;
+		orderQueryVO.arriveBetween = arriveBetween;
+		orderQueryVO.userId = courierId;
+		orderQueryVO.areaCode = user.getSite().getAreaCode();
+		//查询数据
+		PageModel<Order> orderPage = orderService.findPageOrders(pageIndex, orderQueryVO);
 		return orderPage;
 	}
 	
@@ -113,10 +103,12 @@ public class PackageDispatchController {
 	 */
 	@ResponseBody
 	@RequestMapping(value="/dispatch", method=RequestMethod.GET)
-	public Map dispatch(String mailNum, String courierId, Model model) {
+	public Map dispatch(String mailNum, String courierId, final HttpServletRequest request, Model model) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		
 		//====================start================================
+		//当前登录的用户信息
+		User user = adminService.get(UserSession.get(request));
 		//查询运单信息
 		Order order = orderService.findOneByMailNum("", mailNum);
 		if(order != null){
@@ -175,15 +167,6 @@ public class PackageDispatchController {
 		User user = adminService.get(UserSession.get(request));//当前登录的用户信息
 		//查询
 		List<UserVO> userVoList = userService.findUserListBySite(user.getSite().getAreaCode());
-		/*if(userVoList == null || userVoList.size() == 0){
-			UserVO uservo = new UserVO();
-			//uservo.setId(new ObjectId("5546548"));
-			uservo.setRealName("张三");
-			uservo.setLoginName("loginName");
-			uservo.setPhone("12345678945");
-			userVoList.add(uservo);
-		}*/
 		return userVoList;
 	}
-
 }
