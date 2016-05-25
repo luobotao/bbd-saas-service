d59cf8f04af4452dfe180723dfe6b0be65c05a19
botaoluo <botaoluo@gmail.com>
2016-05-25 16:29:10 +0800

    t

--- bbd-saas-consumer/src/main/java/com/bbd/saas/controllers/orderParcel/PackageToSiteController.java
+++ bbd-saas-consumer/src/main/java/com/bbd/saas/controllers/orderParcel/PackageToSiteController.java
@@ -1,6 +1,7 @@
 package com.bbd.saas.controllers.orderParcel;
 
 import com.bbd.drivers.api.mongo.OrderTrackService;
+import com.bbd.drivers.enums.TransStatus;
 import com.bbd.drivers.mongoModels.OrderTrack;
 import com.bbd.saas.Services.AdminService;
 import com.bbd.saas.api.mongo.OrderParcelService;
@@ -56,8 +57,8 @@ public class PackageToSiteController {
 	 * 根据运单号检查是否存在此订单
 	 * @param request
 	 * @param mailNum
-     * @return
-     */
+	 * @return
+	 */
 	@ResponseBody
 	@RequestMapping(value="/checkOrderByMailNum", method=RequestMethod.GET)
 	public Order checkOrderByMailNum(HttpServletRequest request,@RequestParam(value = "mailNum", required = true) String mailNum) {
@@ -69,7 +70,7 @@ public class PackageToSiteController {
 	 * 重新计算到站的数据统计
 	 * @param request
 	 * @return
-     */
+	 */
 	@ResponseBody
 	@RequestMapping(value="/updateOrderNumVO", method=RequestMethod.GET)
 	public OrderNumVO updateOrderNumVO(HttpServletRequest request) {
@@ -80,8 +81,8 @@ public class PackageToSiteController {
 	 * 根据包裹号检查当前登录用户是否有此包裹
 	 * @param request
 	 * @param parcelCode
-     * @return
-     */
+	 * @return
+	 */
 	@ResponseBody
 	@RequestMapping(value="/checkOrderParcelByParcelCode", method=RequestMethod.GET)
 	public boolean checkOrderParcelByParcelCode(HttpServletRequest request,@RequestParam(value = "parcelCode", required = true) String parcelCode) {
@@ -101,8 +102,8 @@ public class PackageToSiteController {
 	 * @param between
 	 * @param parcelCode
 	 * @param mailNum
-     * @return
-     */
+	 * @return
+	 */
 	@ResponseBody
 	@RequestMapping(value = "/getOrderPage", method = RequestMethod.GET)
 	public PageModel<Order> getOrderPage(HttpServletRequest request,Integer pageIndex, Integer arriveStatus,String between,String parcelCode, String mailNum) {
@@ -133,8 +134,8 @@ public class PackageToSiteController {
 	/**
 	 * 批量到站
 	 * @param ids
-     * @return
-     */
+	 * @return
+	 */
 	@ResponseBody
 	@RequestMapping(value = "/arriveBatch", method = RequestMethod.POST)
 	public boolean arriveBatch(HttpServletRequest request,String ids) {
@@ -154,7 +155,7 @@ public class PackageToSiteController {
 	 * 单个订单到站方法
 	 * @param order
 	 * @param user
-     */
+	 */
 	public void orderToSite(Order order,User user) {
 		orderService.updateOrderOrderStatu(order.getMailNum(), OrderStatus.NOTARR, OrderStatus.NOTDISPATCH);//先更新订单本身状态同时会修改该订单所处包裹里的订单状态
 		order = orderService.findOneByMailNum(user.getSite().getAreaCode(), order.getMailNum().toString());
@@ -199,6 +200,7 @@ public class PackageToSiteController {
 							if (flagForUpdateTrackNo) {//可以更新orderTrack下的状态
 								orderTrack.dateUpd = new Date();
 								orderTrack.sendStatus = OrderTrack.SendStatus.ArriveStation;
+								orderTrack.transStatus = TransStatus.YWC;
 								orderTrackService.updateOrderTrack(trackNo, orderTrack);
 								incomeService.driverIncome(Numbers.parseInt(orderTrack.driverId, 0), orderTrack.actOrderPrice, orderTrack.trackNo);
 							}
@@ -217,7 +219,7 @@ public class PackageToSiteController {
 	 * 2016年4月1日下午6:13:46
 	 * @author: liyanlei
 	 * @param model
-	 * @return 
+	 * @return
 	 */
 	@RequestMapping(value="", method=RequestMethod.GET)
 	public String index(Model model,HttpServletRequest request) {
