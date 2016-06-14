a0da410b9086d3e7d6c8acf52c6f57ad911a93bb
chentaotao <kaiwen0930@126.com>
2016-06-14 23:35:46 +0800

    update by ctt

--- bbd-saas-api/src/main/java/com/bbd/saas/vo/ExpressExchangeVO.java
+++ bbd-saas-api/src/main/java/com/bbd/saas/vo/ExpressExchangeVO.java
@@ -2,6 +2,7 @@ package com.bbd.saas.vo;
 
 import com.bbd.saas.enums.ExpressStatus;
 import com.bbd.saas.enums.OrderStatus;
+import com.bbd.saas.enums.Srcs;
 import com.bbd.saas.mongoModels.Order;
 import com.bbd.saas.mongoModels.Site;
 import com.bbd.saas.mongoModels.User;
@@ -16,7 +17,8 @@ import java.util.Date;
 public class ExpressExchangeVO implements Serializable{
 	public String url;                                  //请求url
 	public String operator;                             //操作人
-	public Order order;                                 //订单
+	//public Order order;                                 //订单
+	public Srcs src;									//来源
 	public String typ;                                  //操作类型
 	public String pushInfo;                             //推送内容，json字符串
 	public String requestStr;                           //请求串
