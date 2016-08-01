09fdddf3deb252775ec61b7fda26f7af92909789
luobotao <botaoluo@gmail.com>
2016-08-01 14:57:04 +0800

    测试数据

--- bbd-saas-provider/src/test/java/com/bbt/demo/provider/OrderToOtherCompanyTest.java
+++ bbd-saas-provider/src/test/java/com/bbt/demo/provider/OrderToOtherCompanyTest.java
@@ -2,7 +2,15 @@ package com.bbt.demo.provider;
 
 import com.bbd.saas.api.mongo.OrderService;
 import com.bbd.saas.mongoModels.Order;
+import com.bbd.saas.utils.ExcelReader;
 import com.bbd.saas.utils.ExcelUtil2007;
+import com.google.common.collect.Lists;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,10 +24,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,95 +34,40 @@ public class OrderToOtherCompanyTest {
 
 	@Autowired
 	private OrderService orderService;
-	private ObjectId uId = new ObjectId("573c5f421e06c8275c08183c");
-	//junit.framework.TestCase时用
-	public void setUp() throws Exception{
-        System.out.println("set up");
-        // 生成成员变量的实例
-    }
-	//junit.framework.TestCase时用
-    public void tearDown() throws Exception{
-        System.out.println("tear down");
-    }
+
 	@Test
 	public void testReadExcel() throws Exception{
-		//readOrderInfo(1, 2, 4, 6);//sheet1
-		readOrderInfo(2, 1, 3, 52);//sheet2
-		Assert.isTrue(true);//无用
+		readOrderInfo(0);//sheet2
 	}
-	private void readOrderInfo(int sheetIndex, int areaCodeIndex, int nameIndex, int totalRows) throws FileNotFoundException {
+	private void readOrderInfo(int sheetIndex) throws IOException {
 		// 对读取Excel表格内容测试
 		ExcelUtil2007 excelReader = new ExcelUtil2007();
-		InputStream is = new FileInputStream("E:\\updateSite\\54.xlsx");
-		List<List<String>> rowList = excelReader.readExcelContent(is, sheetIndex, areaCodeIndex, nameIndex, totalRows);
-		System.out.println("获得Excel表格的内容:");
-		StringBuffer  rowSB = null;
-		int size = rowList.size();
-		Map<Integer, List<String>> map = new HashMap<Integer, List<String>>();
-		for (int i = 0; i < size; i++) {
-			List<String> row = rowList.get(i);
-			rowSB = new StringBuffer(i+"");
-			List<String> addressList = getDataByMailNum(row.get(0));
-			map.put(i+1, addressList);
-			if(addressList != null && addressList.size() > 0){
-				for(String col : addressList){
-					rowSB.append("   ");
-					rowSB.append(col);
-					rowSB.append("   ");
-				}
+		InputStream is = new FileInputStream("/Users/Axu/Desktop/7.xlsx");
+		List<List<String>> rowList = Lists.newArrayList();
+
+		HSSFWorkbook wb = new HSSFWorkbook(is);
+		HSSFSheet sheet = wb.getSheetAt(sheetIndex);
+		// 得到总行数
+		int rowNum = sheet.getLastRowNum();
+		System.out.println("行数：" + rowNum);
+		// 正文内容应该从第二行开始,第一行为表头的标题
+		for (int i = 1; i <= rowNum; i++) {
+			HSSFRow row = sheet.getRow(i);
+			String mailNum = ExcelReader.getStringCellValue(row.getCell(1));
+			Order order = orderService.findOneByMailNum(mailNum);
+			if(order!=null){
+				String arriveDate = ExcelReader.getDateCellValue(row.getCell(1));
+				logger.info(arriveDate+"");
 			}
-			System.out.println(rowSB.toString());
+
 		}
+
+
+
+
 	}
-	@Test
-	public void testUpdateExcel() throws Exception{
-		//addAddress(1, 2, 4, 6, 8);//sheet1
-		addAddress(2, 1, 3, 52, 6);//sheet2
-		Assert.isTrue(true);//无用
-	}
-	private void addAddress(int sheetIndex, int areaCodeIndex, int nameIndex, int totalRows, int startCol ) throws IOException {
-		// 对读取Excel表格内容测试
-		ExcelUtil2007 excelReader = new ExcelUtil2007();
-		String filePath = "E:\\updateSite\\6月份深圳BBD各站数据.xlsx";
-		InputStream is = new FileInputStream(filePath);
-		List<List<String>> rowList = excelReader.readExcelContent(is, sheetIndex, areaCodeIndex, nameIndex, totalRows);
-		System.out.println("获得Excel表格的内容:");
-		int size = rowList.size();
-		Map<Integer, List<String>> map = new HashMap<Integer, List<String>>();
-		map.put(0, getTitle());//标题
-		for (int i = 0; i < size; i++) {
-			List<String> row = rowList.get(i);
-			List<String> addressList = getDataByMailNum(row.get(0));
-			map.put(i+1, addressList);
-		}
-		int [] colWidths = new int[]{5000, 3000, 3500};
-		excelReader.creat2007Excel(filePath, sheetIndex, startCol, colWidths, map);
-		is.close();
-	}
-	private List<String> getTitle(){
-		List<String> titleList = new ArrayList<>();
-		titleList.add("原单号");
-		titleList.add("ExpressStatusName");
-		titleList.add("物流状态");
-		return  titleList;
-	}
-	@Test
-	public void testOneUpdate() throws Exception{
-		System.out.println(getDataByMailNum("BBD186866451"));
-		System.out.println(getDataByMailNum("2008000000005"));
-		Assert.isTrue(true);//无用
-	}
-	private List<String> getDataByMailNum(String mailNum){
-		Order order = orderService.findOneByMailNum("", mailNum);
-		if(order != null){
-			List<String> statusList = new ArrayList<String>();
-			statusList.add(mailNum);
-			statusList.add(order.getExpressStatus().toString());
-			statusList.add(order.getExpressStatus().getMessage());
-			return statusList;
-		}
-		return null;
-	}
+
+
 
 
 
