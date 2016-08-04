10d8907415a5d6beffffa7c0e1e9801da130727a
luobotao <botaoluo@gmail.com>
2016-08-04 13:26:48 +0800

    city

--- bbd-saas-provider/src/main/java/com/bbd/saas/api/impl/mongo/OrderParcelServiceImpl.java
+++ bbd-saas-provider/src/main/java/com/bbd/saas/api/impl/mongo/OrderParcelServiceImpl.java
@@ -3,6 +3,7 @@ package com.bbd.saas.api.impl.mongo;
 
 import com.bbd.saas.api.mongo.OrderParcelService;
 import com.bbd.saas.dao.mongo.OrderParcelDao;
+import com.bbd.saas.enums.ParcelStatus;
 import com.bbd.saas.mongoModels.OrderParcel;
 import org.springframework.stereotype.Service;
 
@@ -56,6 +57,16 @@ public class OrderParcelServiceImpl implements OrderParcelService {
 	}
 
 	/**
+	 * 根据订单的运单号查询该运单号所处的包裹
+	 * @param mailNum
+	 * @return
+	 */
+	@Override
+	public OrderParcel findOrderParcelByMailNum(String mailNum) {
+		return orderParcelDao.findOrderParcelByMailNum(mailNum);
+	}
+
+	/**
 	 * 保存包裹
 	 * @param orderParcel
      */
@@ -73,5 +84,15 @@ public class OrderParcelServiceImpl implements OrderParcelService {
 	public List<OrderParcel> findOrderParcelListByTrackCode(String trackNo) {
 		List<OrderParcel> orderParcelList =  orderParcelDao.findOrderParcelListByTrackCode(trackNo);
 		return orderParcelList;
+	}
+	/**
+	 * 根据站点编码和包裹状态获取包裹列表
+	 * @param areaCode
+	 * @param parcelStatus
+	 * @return
+	 */
+	@Override
+	public List<OrderParcel> findOrderParcelsByAreaCodeAndStatus(String areaCode, ParcelStatus parcelStatus) {
+		return  orderParcelDao.findOrderParcelsByAreaCodeAndStatus(areaCode,parcelStatus);
 	}
 }
