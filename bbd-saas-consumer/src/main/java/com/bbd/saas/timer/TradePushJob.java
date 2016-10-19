46f2c206f8ff0d8a1ec7c8ee7a6d26befbd4c651
liyanei <liyanlei@neolix.cn>
2016-10-19 15:13:49 +0800

    关闭推单

--- bbd-saas-consumer/src/main/java/com/bbd/saas/timer/TradePushJob.java
+++ bbd-saas-consumer/src/main/java/com/bbd/saas/timer/TradePushJob.java
@@ -4,7 +4,6 @@ import com.bbd.saas.api.mongo.TradeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,7 +16,7 @@ public class TradePushJob {
     TradeService tradeService;
 
     //每隔60秒跑一次
-    @Scheduled(cron = "*/60 * * * * ?")
+    //@Scheduled(cron = "*/60 * * * * ?")
     public void doJobWithPushAllTrade() {
         logger.info("运单数据推送给揽件员");
         tradeService.doJobWithAllPushTrade();//抢单
