00befdc209e0cb6d776567870f3d6287139a0af8
liyanei <liyanlei@neolix.cn>
2016-11-29 15:45:05 +0800

    显示处罚结果

---
+++ bbd-saas-provider/src/main/java/com/bbd/saas/api/impl/mysql/ComplaintDealServiceImpl.java
@@ -0,0 +1,62 @@
+package com.bbd.saas.api.impl.mysql;
+
+import com.bbd.saas.api.mysql.ComplaintDealService;
+import com.bbd.saas.dao.mysql.ComplaintDealDao;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by liyanlei on 2016/11/29.
+ */
+@Service("complaintDealService")
+@Transactional
+public class ComplaintDealServiceImpl implements ComplaintDealService {
+    @Resource
+    private ComplaintDealDao complaintDealDao;
+
+    public ComplaintDealDao getComplaintDealDao() {
+        return complaintDealDao;
+    }
+
+    public void setComplaintDealDao(ComplaintDealDao complaintDealDao) {
+        this.complaintDealDao = complaintDealDao;
+    }
+
+    @Override
+    public Map<String, String> findListByMailNums(List<String> mailNumList) {
+        List<Map<String, Object>> complaintDealVOList = this.complaintDealDao.selectListByMailNums(mailNumList);
+        if(complaintDealVOList == null || complaintDealVOList.isEmpty()){
+            return null;
+        }
+        Map<String, String> map = new HashMap<String, String>();
+        StringBuffer dealSB = null;
+        int amount;
+        double score;
+        for(Map<String, Object> dealMap : complaintDealVOList){
+            dealSB = new StringBuffer();
+            amount = (Integer)dealMap.get("amount");
+            score = (Double)dealMap.get("score");
+            if(amount > 0){
+                dealSB.append("-");
+                dealSB.append(amount);
+                dealSB.append("元");
+            }
+            if(score > 0){
+                if(dealSB.length() > 0){
+                    dealSB.append(",");
+                }
+                dealSB.append("-");
+                dealSB.append(score);
+                dealSB.append("分");
+            }
+            map.put((String)dealMap.get("mailNum"), dealSB.toString());
+        }
+        return map;
+    }
+}
